a95ebfbbbae0f51407eb6716a5ac2eabe4b70b0b
rodrigodeoliveira19 <rodrigo.oliveira.iupp@zup.com.br>
2022-11-25 03:48:39 -0300

    PostMapping.

--- src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,9 +1,8 @@
 package com.mercadolivro.controller
 
+import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.model.CustomerModel
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/customer")
@@ -12,5 +11,10 @@ class CustomerController {
     @GetMapping
     fun getCustomer() : CustomerModel {
         return CustomerModel("1","Rodrigo","r@gmail.com")
+    }
+
+    @PostMapping
+    fun create(@RequestBody postCustomerRequest: PostCustomerRequest):  PostCustomerRequest{
+        return postCustomerRequest
     }
 }

722e80341c411caab21a9a46db1cb4c9b51ea9c5
rodrigodeoliveira19 <rodrigo.oliveira.iupp@zup.com.br>
2022-11-25 03:31:10 -0300

    Controller - GetMapping

---
+++ src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,16 @@
+package com.mercadolivro.controller
+
+import com.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer() : CustomerModel {
+        return CustomerModel("1","Rodrigo","r@gmail.com")
+    }
+}
